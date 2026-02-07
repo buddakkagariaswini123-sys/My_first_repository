7c343159f5bd94152bcdda421c6f4c5743caae01
buddakkagariaswini123-sys <buddakkagariaswini123@gmail.com>
2026-02-07 23:40:50 +0530

    Task_1.py
     I had Developed a command-line calculator that can perform basic arithmetic operations: addition, subtraction, multiplication, and division. The program will take user input for numbers and the desired operation, process it using conditional statements, and display the result.

---
+++ calculator(Task_1).py
@@ -0,0 +1,20 @@
+﻿
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Choose operation: +  -  *  /")
+op = input("Enter operation: ")
+
+if op == "+":
+    print("Result:", a + b)
+elif op == "-":
+    print("Result:", a - b)
+elif op == "*":
+    print("Result:", a * b)
+elif op == "/":
+    if b == 0:
+        print("Cannot divide by zero")
+    else:
+        print("Result:", a / b)
+else:
+    print("Invalid operation")
